414eb59bff12d6d704dd4f18a09c6fe7f5df7b07
Akmalkhon Burkhonov <akmalburhonov5515@gmail.com>
2024-09-02 09:24:46 +0500

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by itserviceimac on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
